53c992cf216ec6117bd45c7c401d6d3c0b26e50e
crazy-max <webmaster@crazyws.fr>
2017-04-09 22:55:03 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,47 @@
+<p align="center"><a href="https://github.com/crazy-max/slack-portable" target="_blank"><img width="100" src="https://github.com/crazy-max/slack-portable/blob/master/res/logo.png"></a></p>
+
+<p align="center">
+  <a href="https://github.com/crazy-max/slack-portable/releases/latest"><img src="https://img.shields.io/github/release/crazy-max/slack-portable.svg?style=flat-square" alt="GitHub release"></a>
+  <a href="https://ci.appveyor.com/project/crazy-max/slack-portable"><img src="https://img.shields.io/appveyor/ci/crazy-max/slack-portable.svg?style=flat-square" alt="AppVeyor"></a>
+  <a href="https://www.codacy.com/app/crazy-max/slack-portable"><img src="https://img.shields.io/codacy/grade/8beee2b3463842f6ad27da362666e75c.svg?style=flat-square" alt="Code Quality"></a>
+  <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=4Y9DDQ8LDLMTL"><img src="https://img.shields.io/badge/donate-paypal-blue.svg?style=flat-square" alt="Donate Paypal"></a>
+  <a href="https://flattr.com/submit/auto?user_id=crazymax&url=https://github.com/crazy-max/slack-portable"><img src="https://img.shields.io/badge/flattr-this-green.svg?style=flat-square" alt="Flattr this!"></a>
+</p>
+
+## About
+
+A single EXE made with [Go](https://golang.org/) to make [Slack](https://slack.com) portable on Windows systems.<br />
+Tested on Windows 7, Windows 8.1 and Windows 10.
+
+## Requirements
+
+* [Microsoft .NET Framework 4.5](https://www.microsoft.com/en-us/download/details.aspx?id=30653) for Slack.
+
+## Installation
+
+For a fresh installation, download and extract the [latest release](https://github.com/crazy-max/slack-portable/releases/latest) where you want then run `slack-portable.exe`.
+
+If you have already installed Slack on your computer from the original setup, do the same thing as a fresh installation and run `slack-portable.exe` a first time. The data located in `%APPDATA%\Slack` will be move the `data` folder. Then you can remove Slack from your computer.
+
+## Upgrade
+
+Download and overwrite to the latest `slack-portable.exe`.
+
+## How can i help ?
+
+I'm a single developer and if you find this project useful, please consider making a donation.<br />
+Any funds donated will be used to help further development on this project! :gift_heart:
+
+<p>
+  <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=4Y9DDQ8LDLMTL">
+    <img src="https://github.com/crazy-max/slack-portable/blob/master/res/paypal.png" alt="Donate Paypal">
+  </a>
+  <a href="https://flattr.com/submit/auto?user_id=crazymax&url=https://github.com/crazy-max/slack-portable">
+    <img src="https://github.com/crazy-max/slack-portable/blob/master/res/flattr.png" alt="Flattr this!">
+  </a>
+</p>
+
+## License
+
+LGPL. See `LICENSE` for more details.<br />
+USB icon credit to [Juliia Osadcha](https://www.iconfinder.com/Juliia_Os).
---
+++ slack-portable.go
@@ -0,0 +1,238 @@
+//go:generate goversioninfo -icon=slack-portable.ico
+package main
+
+import (
+  "encoding/json"
+  "fmt"
+  "io"
+  "io/ioutil"
+  "os"
+  "os/exec"
+  "path"
+  "path/filepath"
+  "strings"
+  "syscall"
+  
+  "github.com/op/go-logging"
+)
+
+var log = logging.MustGetLogger("slack-portable")
+var logFormat = logging.MustStringFormatter(`%{time} %{level:.4s} - %{message}`)
+
+func main() {
+  // Current path
+  currentPath, err := filepath.Abs(filepath.Dir(os.Args[0]))
+  if err != nil {
+    log.Error(err)
+  }
+  
+  // Log file
+  logfile, err := os.OpenFile(path.Join(currentPath, "slack-portable.log"), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+  if err != nil {
+    log.Error(err)
+  }
+  
+  // Init logger
+  logBackendStdout := logging.NewBackendFormatter(logging.NewLogBackend(os.Stdout, "", 0), logFormat)
+  logBackendFile := logging.NewBackendFormatter(logging.NewLogBackend(logfile, "", 0), logFormat)
+  logging.SetBackend(logBackendStdout, logBackendFile)
+  log.Info("--------")
+  log.Info("Starting slack-portable...")
+  
+  // Convert backslashes
+  currentPath = path.Join(strings.Replace(string(currentPath), string(filepath.Separator), "/", -1))
+  log.Info("Current path: " + currentPath)
+  
+  // Find app folder
+  var appPath = ""
+  rootFiles, _ := ioutil.ReadDir(currentPath)
+  for _, f := range rootFiles {
+    if (strings.HasPrefix(f.Name(), "app-") && f.IsDir()) {
+      appPath = path.Join(currentPath, f.Name())
+      break
+    }
+  }
+  if _, err := os.Stat(appPath); err == nil {
+    log.Info("App path: " + appPath)
+  } else {
+    log.Warning("App path does not exist");
+  }
+  
+  // Init vars
+  var slackExe = path.Join(currentPath, "slack.exe")
+  var dataPath = path.Join(currentPath, "data")
+  var downloadsPath = path.Join(currentPath, "downloads")
+  var symlinkPath = path.Clean(path.Join(os.Getenv("APPDATA"), "Slack"))
+  //var symlinkPath = path.Join(currentPath, "data2")
+  var slackSettingsPath = path.Join(dataPath, "storage", "slack-settings")
+  log.Info("Slack executable: " + slackExe)
+  log.Info("Data path: " + dataPath)
+  log.Info("Downloads path: " + downloadsPath)
+  log.Info("Symlink path: " + symlinkPath)
+  log.Info("Slack settings path: " + slackSettingsPath)
+  
+  // Check data folder
+  if _, err := os.Stat(symlinkPath); err == nil {
+    if _, err := os.Stat(dataPath); os.IsNotExist(err) {
+      os.Mkdir(dataPath, 777)
+      err = copyDir(symlinkPath, dataPath)
+      if err != nil {
+        log.Error(err)
+      }
+    }
+  }
+  
+  // Rename old data folder if not a symlink
+  fi, err := os.Lstat(symlinkPath)
+  if err != nil {
+    log.Error(err)
+  }
+  if fi.Mode() & os.ModeSymlink != os.ModeSymlink {
+    err = os.Chmod(symlinkPath, 0777)
+    if err != nil {
+      log.Error(err)
+    }
+    err = os.Rename(symlinkPath, symlinkPath + "_old")
+    if err != nil {
+      log.Error(err)
+    }
+  }
+  
+  // Create symlink
+  os.Remove(symlinkPath)
+  cmd := exec.Command("cmd", "/c", "mklink", "/J", strings.Replace(symlinkPath, "/", "\\", -1), strings.Replace(dataPath, "/", "\\", -1))
+  cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+  if err := cmd.Run(); err != nil { 
+    log.Error("Create symlink:", err)
+  }
+  /*err = os.Symlink(dataPath, symlinkPath)
+  if err != nil {
+    log.Error(err)
+  }*/
+  
+  // Check downloads folder
+  if _, err := os.Stat(downloadsPath); os.IsNotExist(err) {
+    os.Mkdir(downloadsPath, 777)
+  }
+  
+  // Change slack settings
+  if _, err := os.Stat(slackSettingsPath); err == nil {
+    rawSettings, err := ioutil.ReadFile(slackSettingsPath)
+    if err == nil {
+      jsonMapSettings := make(map[string]interface{})
+      json.Unmarshal(rawSettings, &jsonMapSettings)
+      log.Info("Current settings:", jsonMapSettings)
+      
+      jsonMapSettings["resourcePath"] = strings.Replace(appPath + "/resources/app.asar", "/", "\\", -1)
+      jsonMapSettings["PrefSSBFileDownloadPath"] = strings.Replace(downloadsPath, "/", "\\", -1)
+      log.Info("New settings:", jsonMapSettings)
+      
+      jsonSettings, err := json.Marshal(jsonMapSettings)
+      if err != nil {
+        log.Error(err)
+      }
+      err = ioutil.WriteFile(slackSettingsPath, jsonSettings, 0644)
+      if err != nil {
+        log.Error(err)
+      }
+    }
+  } else {
+    log.Warning("Slack settings not found in " + slackSettingsPath)
+  }
+  
+  // Launch slack
+  cmd = exec.Command(slackExe)
+  if err := cmd.Run(); err != nil { 
+    log.Error(err)
+  }
+  
+  defer logfile.Close()
+}
+
+// src: https://gist.github.com/m4ng0squ4sh/92462b38df26839a3ca324697c8cba04
+func copyFile(src, dst string) (err error) {
+  in, err := os.Open(src)
+  if err != nil {
+    return
+  }
+  defer in.Close()
+
+  out, err := os.Create(dst)
+  if err != nil {
+    return
+  }
+  defer func() {
+    if e := out.Close(); e != nil {
+      err = e
+    }
+  }()
+
+  _, err = io.Copy(out, in)
+  if err != nil {
+    return
+  }
+
+  err = out.Sync()
+  if err != nil {
+    return
+  }
+
+  si, err := os.Stat(src)
+  if err != nil {
+    return
+  }
+  
+  err = os.Chmod(dst, si.Mode())
+  if err != nil {
+    return
+  }
+
+  return
+}
+
+// src: https://gist.github.com/m4ng0squ4sh/92462b38df26839a3ca324697c8cba04
+func copyDir(src string, dst string) (err error) {
+  src = filepath.Clean(src)
+  dst = filepath.Clean(dst)
+
+  si, err := os.Stat(src)
+  if err != nil {
+    return err
+  }
+  if !si.IsDir() {
+    return fmt.Errorf("source is not a directory")
+  }
+
+  _, err = os.Stat(dst)
+  if err != nil && !os.IsNotExist(err) {
+    return
+  }
+
+  err = os.MkdirAll(dst, si.Mode())
+  if err != nil {
+    return
+  }
+
+  entries, err := ioutil.ReadDir(src)
+  if err != nil {
+    return
+  }
+
+  for _, entry := range entries {
+    srcPath := filepath.Join(src, entry.Name())
+    dstPath := filepath.Join(dst, entry.Name())
+    if entry.IsDir() {
+      err = copyDir(srcPath, dstPath)
+      if err != nil {
+        return
+      }
+    } else {
+      err = copyFile(srcPath, dstPath)
+      if err != nil {
+        return
+      }
+    }
+  }
+
+  return
+}
