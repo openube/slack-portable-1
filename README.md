cc67f41f8acd526eeb9a4df0006a0c543fef92fc
CrazyMax <crazy-max@users.noreply.github.com>
2017-11-19 17:52:59 +0100

    Move repository to Portapps org

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
-<p align="center"><a href="https://github.com/crazy-max/slack-portable" target="_blank"><img width="100" src="https://github.com/crazy-max/slack-portable/blob/master/res/logo.png"></a></p>
+<p align="center"><a href="https://github.com/portapps/slack-portable" target="_blank"><img width="100" src="https://github.com/portapps/slack-portable/blob/master/res/papp.png"></a></p>
 
 <p align="center">
-  <a href="https://github.com/crazy-max/slack-portable/releases/latest"><img src="https://img.shields.io/github/release/crazy-max/slack-portable.svg?style=flat-square" alt="GitHub release"></a>
-  <a href="https://github.com/crazy-max/slack-portable/releases/latest"><img src="https://img.shields.io/github/downloads/crazy-max/slack-portable/total.svg?style=flat-square" alt="Total downloads"></a>
-  <a href="https://ci.appveyor.com/project/crazy-max/slack-portable"><img src="https://img.shields.io/appveyor/ci/crazy-max/slack-portable.svg?style=flat-square" alt="AppVeyor"></a>
-  <a href="https://goreportcard.com/report/github.com/crazy-max/slack-portable"><img src="https://goreportcard.com/badge/github.com/crazy-max/slack-portable?style=flat-square" alt="Go Report"></a>
-  <a href="https://www.codacy.com/app/crazy-max/slack-portable"><img src="https://img.shields.io/codacy/grade/8beee2b3463842f6ad27da362666e75c.svg?style=flat-square" alt="Code Quality"></a>
-  <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=4Y9DDQ8LDLMTL"><img src="https://img.shields.io/badge/donate-paypal-7057ff.svg?style=flat-square" alt="Donate Paypal"></a>
+  <a href="https://github.com/portapps/slack-portable/releases/latest"><img src="https://img.shields.io/github/release/portapps/slack-portable.svg?style=flat-square" alt="GitHub release"></a>
+  <a href="https://github.com/portapps/slack-portable/releases/latest"><img src="https://img.shields.io/github/downloads/portapps/slack-portable/total.svg?style=flat-square" alt="Total downloads"></a>
+  <a href="https://ci.appveyor.com/project/portapps/slack-portable"><img src="https://img.shields.io/appveyor/ci/crazy-max/slack-portable.svg?style=flat-square" alt="AppVeyor"></a>
+  <a href="https://goreportcard.com/report/github.com/portapps/slack-portable"><img src="https://goreportcard.com/badge/github.com/portapps/slack-portable?style=flat-square" alt="Go Report"></a>
+  <a href="https://www.codacy.com/app/portapps/slack-portable"><img src="https://img.shields.io/codacy/grade/8beee2b3463842f6ad27da362666e75c.svg?style=flat-square" alt="Code Quality"></a>
+  <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=WQD7AQGPDEPSG"><img src="https://img.shields.io/badge/donate-paypal-7057ff.svg?style=flat-square" alt="Donate Paypal"></a>
 </p>
 
 ## About
 
-A single EXE written in [Golang](https://golang.org/) to make [Slack](https://slack.com) portable on Windows systems.<br />
+[Slack](https://slack.com) portable app made with 🚀 [Portapps](https://github.com/portapps).<br />
 Tested on Windows 7, Windows 8.1 and Windows 10.
 
 ## Installation
@@ -29,7 +29,7 @@ For a **fresh installation**, install `slack-portable-{ia32,x64}-x.x.x-x-setup.e
 If **you have already installed Slack from the original setup**, do the same thing as a fresh installation and move the data located in `%APPDATA%\slack` to `data\AppData\Roaming\slack` then run `slack-portable.exe`.<br />
 Then you can [remove](https://support.microsoft.com/en-us/instantanswers/ce7ba88b-4e95-4354-b807-35732db36c4d/repair-or-remove-programs) Slack from your computer.
 
-**For an upgrade**, simply download and install the [latest setup](https://github.com/crazy-max/slack-portable/releases/latest).
+**For an upgrade**, simply download and install the [latest setup](https://github.com/portapps/slack-portable/releases/latest).
 
 ## How can i help ?
 
@@ -37,9 +37,9 @@ We welcome all kinds of contributions :raised_hands:!<br />
 The most basic way to show your support is to star :star2: the project, or to raise issues :speech_balloon:<br />
 Any funds donated will be used to help further development on this project! :gift_heart:
 
-[![Donate Paypal](https://raw.githubusercontent.com/crazy-max/slack-portable/master/res/paypal.png)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=4Y9DDQ8LDLMTL)
+[![Donate Paypal](https://raw.githubusercontent.com/portapps/portapps/master/res/paypal.png)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=WQD7AQGPDEPSG)
 
 ## License
 
 MIT. See `LICENSE` for more details.<br />
-USB icon credit to [Juliia Osadcha](https://www.iconfinder.com/Juliia_Os).
+Rocket icon credit to [Squid Ink](http://thesquid.ink).
--- main.go
+++ main.go
@@ -1,165 +1,59 @@
 //go:generate go get -v github.com/josephspurrier/goversioninfo/...
-//go:generate goversioninfo -icon=res/app-portable.ico
+//go:generate goversioninfo -icon=res/papp.ico
 package main
 
 import (
 	"encoding/json"
 	"io/ioutil"
 	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
 
-	"github.com/google/logger"
+	. "github.com/portapps/portapps"
 )
 
-const (
-	NAME            = "slack-portable"
-	APP_NAME        = "Slack"
-	APP_DATA_FOLDER = "slack"
-	APP_PROCESS     = "slack.exe"
-)
+func init() {
+	App.Id = "slack-portable"
+	App.Name = "Slack"
+	Init()
+}
 
 func main() {
-	// Current path
-	currentPath, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		logger.Error("Current path:", err)
-	}
+	App.MainPath = FindElectronMainFolder("app-")
+	App.DataPath = CreateFolder(PathJoin(App.RootDataPath, "AppData", "Roaming", "Slack"))
+	App.Process = RootPathJoin("Slack.exe")
+	App.Args = nil
+	App.WorkingDir = App.MainPath
 
-	// Logs folder
-	logsPath := pathJoin(currentPath, "logs")
-	logger.Info("Create logs folder", logsPath)
-	err = os.Mkdir(logsPath, 777)
-	if err != nil {
-		logger.Error("Create logs folder:", err)
-	}
+	// Downloads folder
+	downloadsPath := CreateFolder(PathJoin(App.Path, "downloads"))
 
-	// Log file
-	logfile, err := os.OpenFile(pathJoin(logsPath, NAME+".log"), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-	if err != nil {
-		logger.Error("Log file:", err)
-	}
-
-	// Init logger
-	logger.Init(NAME, false, false, logfile)
-	logger.Info("--------")
-	logger.Infof("Starting %s...", NAME)
-	logger.Infof("Current path: %s", currentPath)
-
-	// Purge logs
-	logsFolder, err := os.Open(logsPath)
-	if err != nil {
-		logger.Error("Open logs folder:", err)
-	}
-	defer logsFolder.Close()
-	logsFiles, err := logsFolder.Readdir(-1)
-	if err != nil {
-		logger.Error("Read logs folder:", err)
-	}
-	logger.Infof("Reading %s...", logsPath)
-	for _, logsFile := range logsFiles {
-		if !strings.HasPrefix(logsFile.Name(), NAME) {
-			os.Remove(pathJoin(logsPath, logsFile.Name()))
-			logger.Infof("Deleted %s", pathJoin(logsPath, logsFile.Name()))
-		}
-	}
-
-	// Find app folder
-	logger.Infof("Lookup app folder in %s", currentPath)
-	var appPath = ""
-	rootFiles, _ := ioutil.ReadDir(currentPath)
-	for _, f := range rootFiles {
-		if strings.HasPrefix(f.Name(), "app-") && f.IsDir() {
-			logger.Infof("App folder found: %s", f.Name())
-			appPath = pathJoin(currentPath, f.Name())
-			break
-		}
-	}
-	if _, err := os.Stat(appPath); err == nil {
-		logger.Infof("App path: %s", appPath)
-	} else {
-		logger.Error("App path does not exist")
-	}
-
-	// Init vars
-	appExe := pathJoin(appPath, APP_PROCESS)
-	dataPath := pathJoin(currentPath, "data")
-	dataAppPath := pathJoin(dataPath, "AppData", "Roaming", APP_DATA_FOLDER)
-	downloadsPath := pathJoin(currentPath, "downloads")
-	slackSettingsPath := pathJoin(dataAppPath, "storage", "slack-settings")
-	logger.Info("App executable:", appExe)
-	logger.Info("Data path:", dataAppPath)
-	logger.Info("Downloads path:", downloadsPath)
-	logger.Info("Slack settings path:", slackSettingsPath)
-
-	// Create data folder
-	logger.Infof("Create data folder %s...", dataAppPath)
-	err = os.MkdirAll(dataAppPath, 777)
-	if err != nil {
-		logger.Error("Create data folder:", err)
-	}
-
-	// Create download folder
-	logger.Infof("Create download folder %s...", downloadsPath)
-	err = os.MkdirAll(downloadsPath, 777)
-	if err != nil {
-		logger.Error("Create download folder:", err)
-	}
-
-	// Override USERPROFILE env var
-	if err := os.Setenv("USERPROFILE", dataPath); err != nil {
-		logger.Error("Cannot set USERPROFILE env var:", err)
-	}
-
-	// Change slack settings
-	logger.Info("Update Slack settings...")
+	// Update slack settings
+	Log.Info("Update Slack settings...")
+	slackSettingsPath := PathJoin(App.DataPath, "storage", "slack-settings")
 	if _, err := os.Stat(slackSettingsPath); err == nil {
 		rawSettings, err := ioutil.ReadFile(slackSettingsPath)
 		if err == nil {
 			jsonMapSettings := make(map[string]interface{})
 			json.Unmarshal(rawSettings, &jsonMapSettings)
-			logger.Info("Current settings:", jsonMapSettings)
+			Log.Info("Current settings:", jsonMapSettings)
 
-			jsonMapSettings["resourcePath"] = pathJoin(appPath, "resources", "app.asar")
+			jsonMapSettings["resourcePath"] = PathJoin(App.MainPath, "resources", "app.asar")
 			jsonMapSettings["PrefSSBFileDownloadPath"] = downloadsPath
 			jsonMapSettings["notificationMethod"] = "html"
-			logger.Info("New settings:", jsonMapSettings)
+			Log.Info("New settings:", jsonMapSettings)
 
 			jsonSettings, err := json.Marshal(jsonMapSettings)
 			if err != nil {
-				logger.Error("Slack settings marshal:", err)
+				Log.Error("Slack settings marshal:", err)
 			}
 			err = ioutil.WriteFile(slackSettingsPath, jsonSettings, 0644)
 			if err != nil {
-				logger.Error("Write Slack settings:", err)
+				Log.Error("Write Slack settings:", err)
 			}
 		}
 	} else {
-		logger.Errorf("Slack settings not found in %s", slackSettingsPath)
+		Log.Errorf("Slack settings not found in %s", slackSettingsPath)
 	}
 
-	// Launch
-	logger.Infof("Launch %s...", APP_NAME)
-	execApp := exec.Command(appExe, "--log-file", "./")
-	execApp.Dir = logsPath
-
-	defer logfile.Close()
-	execApp.Stdout = logfile
-	execApp.Stderr = logfile
-
-	if err := execApp.Start(); err != nil {
-		logger.Error("Cmd Start:", err)
-	}
-
-	execApp.Wait()
-}
-
-func pathJoin(elem ...string) string {
-	for i, e := range elem {
-		if e != "" {
-			return strings.Join(elem[i:], `\`)
-		}
-	}
-	return ""
+	OverrideUserprofilePath(App.RootDataPath)
+	Launch()
 }
