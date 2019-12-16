5e8d3119bd2d5e94f382098c55fc0fcdfa72981e
CrazyMax <crazy-max@users.noreply.github.com>
2019-12-16 18:14:48 +0100

    Add cleanup config

--- main.go
+++ main.go
@@ -16,15 +16,25 @@ import (
 	"github.com/portapps/slack-portable/assets"
 )
 
+type config struct {
+	Cleanup bool `yaml:"cleanup" mapstructure:"cleanup"`
+}
+
 var (
 	app *App
+	cfg *config
 )
 
 func init() {
 	var err error
 
+	// Default config
+	cfg = &config{
+		Cleanup: false,
+	}
+
 	// Init app
-	if app, err = New("slack-portable", "Slack"); err != nil {
+	if app, err = NewWithCfg("slack-portable", "Slack", cfg); err != nil {
 		Log.Fatal().Err(err).Msg("Cannot initialize application. See log file for more info.")
 	}
 }
@@ -38,6 +48,15 @@ func main() {
 		"--user-data-dir=" + app.DataPath,
 	}
 	app.WorkingDir = electronBinPath
+
+	// Cleanup on exit
+	if cfg.Cleanup {
+		defer func() {
+			utl.Cleanup([]string{
+				path.Join(os.Getenv("APPDATA"), "Slack"),
+			})
+		}()
+	}
 
 	// Downloads folder
 	downloadsPath := utl.CreateFolder(app.RootPath, "downloads")
